285adc16822cdf0d1243a03780db108679ad59fe
Zachary C <commonazach@gmail.com>
2020-07-20 12:37:53 +0000

    fix: agenda files

--- lisp/prefs/config-org.el
+++ lisp/prefs/config-org.el
@@ -25,7 +25,24 @@
 
 ;;; Code:
 
+(require 'org-agenda)
 
+(global-set-key (kbd "C-c a") #'org-agenda)
+
+(when (and (not (file-directory-p (concat (expand-file-name "~/Documents/") "Org/")))
+		   (y-or-n-p (concat "Make directory " (concat (expand-file-name "~/Documents/") "Org/") "?")))
+  (if (file-directory-p (expand-file-name "~/Documents/"))
+	  (make-directory (concat (expand-file-name "~/Documents/") "Org/"))
+	(progn
+	  (make-directory (expand-file-name "~/Documents/"))
+	  (make-directory (concat (expand-file-name "~/Documents/") "Org/")))))
+
+(setq org-directory (concat (expand-file-name "~/Documents/") "Org/")
+	  org-todo-keywords '((sequence "TODO(t)" "DOING(p)" "|" "FINISHED(f)" "CANCEL(c)"))
+	  org-support-shift-select t)
+(setq org-agenda-files (list (concat org-directory
+									 (elt (directory-files org-directory nil "\\.org$")
+										  (cl-position "agenda.org" (directory-files org-directory nil "\\.org$") :test 'equal)))))
 
 (use-package org-bullets
   :ensure t
@@ -45,10 +62,10 @@
   :defines org-capture-templates
   :config
   (setq org-capture-templates
-		'(("c" "Cookbook" entry (file "~/org/cookbook.org")
+		'(("c" "Cookbook" entry (file (expand-file-name "~/Documents/Org/cookbook.org"))
 		   "%(org-chef-get-recipe-from-url)"
 		   :empty-lines 1)
-		  ("m" "Manual Cookbook" entry (file "~/org/cookbook.org")
+		  ("m" "Manual Cookbook" entry (file (expand-file-name "~/Documents/Org/cookbook.org"))
 		   "* %^{Recipe title: }\n  :PROPERTIES:\n  :source-url:\n  :servings:\n  :prep-time:\n  :cook-time:\n  :ready-in:\n  :END:\n** Ingredients\n   %?\n** Directions\n\n"))))
 
 (provide 'config-org)
